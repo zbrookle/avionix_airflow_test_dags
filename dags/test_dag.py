f0ed9c5bb700b65c709d3825a8b00f052419695d
zbrookle <zachb1996@yahoo.com>
2020-07-30 13:22:50 -0400

    ENH: Add output to dag

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,6 +1,7 @@
 from airflow.models.dag import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
+from airflow.operators.bash_operator import BashOperator
 
 DAG_NAME = "test"
 default_args = {
@@ -8,10 +9,9 @@ default_args = {
     "depends_on_past": True,
     "start_date": datetime(2020, 1, 1),
 }
-dag = DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args)
-
-run_this_1 = DummyOperator(task_id="run_this_1", dag=dag)
-run_this_2 = DummyOperator(task_id="run_this_2", dag=dag)
-run_this_2.set_upstream(run_this_1)
-run_this_3 = DummyOperator(task_id="run_this_3", dag=dag)
-run_this_3.set_upstream(run_this_2)
+with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) as dag:
+    run_this_1 = DummyOperator(task_id="run_this_1")
+    run_this_2 = DummyOperator(task_id="run_this_2")
+    run_this_2.set_upstream(run_this_1)
+    run_this_3 = BashOperator(task_id="run_this_3", bash_command="echo success")
+    run_this_3.set_upstream(run_this_2)
