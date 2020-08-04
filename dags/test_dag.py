d52af145f67f7d5d392886261cc176baaa6922c7
zbrookle <zachb1996@yahoo.com>
2020-08-04 12:25:58 -0400

    ENH: Add test for service account

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -13,5 +13,9 @@ with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) 
     run_this_1 = DummyOperator(task_id="run_this_1")
     run_this_2 = DummyOperator(task_id="run_this_2")
     run_this_2.set_upstream(run_this_1)
-    run_this_3 = BashOperator(task_id="run_this_3", bash_command="echo success")
+    run_this_3 = BashOperator(
+        task_id="run_this_3",
+        bash_command="echo success",
+        executor_config={"service_account_name": "airflow-pod-account"},
+    )
     run_this_3.set_upstream(run_this_2)
