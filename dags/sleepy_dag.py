2cbac62a40940615c19c7e42c07b9e445235e287
zbrookle <zachb1996@yahoo.com>
2020-08-10 12:13:42 -0400

    ENH: Add dag that will take longer

---
+++ dags/sleepy_dag.py
@@ -0,0 +1,22 @@
+from airflow.models.dag import DAG
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from time import sleep
+
+DAG_NAME = "sleepy_test"
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": True,
+    "start_date": datetime(2020, 1, 1),
+}
+
+
+def sleepy_func():
+    for i in range(5):
+        print("I'm sleepy...")
+        print("zzzzzz...")
+        sleep(1)
+
+
+with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) as dag:
+    sleepy_time = PythonOperator(task_id="sleepy_time", python_callable=sleepy_func)
