74533addf3a5bbbe6ffd4536aa0af851fec13e08
zbrookle <zachb1996@yahoo.com>
2020-08-16 01:43:49 -0400

    TST: Remove depends on past

--- dags/big_daggy.py
+++ dags/big_daggy.py
@@ -5,7 +5,6 @@ from airflow.operators.bash_operator import BashOperator
 DAG_NAME = "big_daggy"
 default_args = {
     "owner": "airflow",
-    "depends_on_past": True,
     "start_date": datetime(2020, 1, 1),
 }
 with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) as dag:
@@ -16,7 +15,6 @@ with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) 
             "KubernetesExecutor": {
                 "resources": {
                     "requests": {"cpu": "0.5", "memory": "200Mi"},
-                    # "request_memory": "200Mi"
                 }
             }
         },
