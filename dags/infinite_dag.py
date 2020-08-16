71be05572b11dc48a801ebfe1909611f6415be09
zbrookle <zachb1996@yahoo.com>
2020-08-15 22:46:15 -0400

    BUG: Make dag infinite

--- dags/infinite_dag.py
+++ dags/infinite_dag.py
@@ -5,7 +5,6 @@ from airflow.operators.bash_operator import BashOperator
 DAG_NAME = "infinite_dag"
 default_args = {
     "owner": "airflow",
-    "depends_on_past": True,
     "start_date": datetime(2020, 1, 1),
 }
 with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) as dag:
