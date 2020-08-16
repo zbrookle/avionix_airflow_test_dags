338c89a22fd0ea6e268d79ff940bb46c29ce650f
zbrookle <zachb1996@yahoo.com>
2020-08-15 23:51:12 -0400

    BUG: Fix executor requests/limits

--- dags/big_daggy.py
+++ dags/big_daggy.py
@@ -2,7 +2,7 @@ from airflow.models.dag import DAG
 from datetime import datetime
 from airflow.operators.bash_operator import BashOperator
 
-DAG_NAME = "big_daggy"
+DAG_NAME = "infinite_dag"
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2020, 1, 1),
@@ -13,8 +13,11 @@ with DAG(DAG_NAME, schedule_interval="*/10 * * * *", default_args=default_args) 
         bash_command="echo success",
         executor_config={
             "KubernetesExecutor": {
-                "requests": {"cpu": "1", "memory": "128Mi"},
-                "limits": {"cpu": "1", "memory": "128Mi"},
+                "resources": {
+                    "request_cpu": "1",
+                    "request_memory": "128Mi",
+                    "limit_memory": "128Mi",
+                }
             }
         },
     )
